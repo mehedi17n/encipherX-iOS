70905659e1313429fdec9b8f666131a9568fef06
Md. Mehdi Hasan <mehedi17n@gmail.com>
2026-03-29 16:58:41 +0600

    Update account provider and push gateway URL to use development server

--- ElementX/Sources/Application/Settings/AppSettings.swift
+++ ElementX/Sources/Application/Settings/AppSettings.swift
@@ -198,7 +198,7 @@ final class AppSettings {
     ///
     /// Account provider is the friendly term for the server name. It should not contain an `https` prefix and should
     /// match the last part of the user ID. For example `example.com` and not `https://matrix.example.com`.
-    private(set) var accountProviders = ["matrix.org"]
+    private(set) var accountProviders = ["dev.enciph-er.com"]
     /// Whether or not the user is allowed to manually enter their own account provider or must select from one of `defaultAccountProviders`.
     private(set) var allowOtherAccountProviders = true
     /// Whether the components surrounding the app brand/logo should be hidden or not
@@ -281,7 +281,7 @@ final class AppSettings {
         #endif
     }
     
-    private(set) var pushGatewayBaseURL: URL = "https://matrix.org"
+    private(set) var pushGatewayBaseURL: URL = "https://dev.enciph-er.com"
     var pushGatewayNotifyEndpoint: URL {
         pushGatewayBaseURL.appending(path: "_matrix/push/v1/notify")
     }
