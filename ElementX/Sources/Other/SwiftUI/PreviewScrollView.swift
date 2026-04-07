fcd604776ce41066000dbd80ee8e438b0099218a
Md. Mehdi Hasan <mehedi17n@gmail.com>
2026-04-07 17:24:34 +0600

    refactor: remove DEBUG conditional compilation from PreviewScrollView to enable usage in non-debug builds

--- ElementX/Sources/Other/SwiftUI/PreviewScrollView.swift
+++ ElementX/Sources/Other/SwiftUI/PreviewScrollView.swift
@@ -7,13 +7,12 @@
 
 import SwiftUI
 
-#if DEBUG
 /// Only use in Previews! This useful scroll view allows you to still have a scroll view when previewing in Xcode
 /// but ignores it when running the tests, which allows you to still use it's content directly in preview tests
 /// and render the preview with `sizeThatFits` layout.
 struct PreviewScrollView<Content: View>: View {
     var content: () -> Content
-    
+
     var body: some View {
         if ProcessInfo.isRunningTests {
             content()
@@ -24,4 +23,3 @@ struct PreviewScrollView<Content: View>: View {
         }
     }
 }
-#endif
